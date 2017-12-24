3a60e2d1c23903a8df33c4f8ce579cb6f3e44b40
Jiacheng03 <chenjiacheng03@163.com>
2017-12-24 12:56:47 +0800

    MemLeakDector, 增加使用示例；

---
+++ win VS/main.cpp
@@ -0,0 +1,23 @@
+﻿//#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+//#include <unordered_map>
+//using namespace std;
+
+#include "MemLeakDetector.h"
+//#include <memory>
+
+void func()
+{
+	char* pp = new char;	
+	//char* pp = (char*)malloc(sizeof(char));
+	//std::shared_ptr<char> pa(pp);	
+}
+
+int main()
+{
+	//X::MemLeakDetector::SetBreakAlloc(75);
+	func();	
+	
+	return 0;
+}
